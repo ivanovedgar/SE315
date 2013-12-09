ff9992709d1c304d506d075bece84c9a4e5cad09
Edgar Ivanov <zajjac@yandex.ru>
2013-12-09 13:47:11 +0000

    Report: testing and conclusion finished

--- underwriter2/app/models/user.rb
+++ underwriter2/app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ActiveRecord::Base
+validates_presence_of :title, :name, :surname, :DOB, :telephoneNumber, :address, :currentLicenseType, :licensePeriod, :occupation, :vehicleRegistration, :estimatedAnnualMileage, :estimatedVehicleValue, :typicalParkingLocation, :policyStartDate, :numberOfIncidents, :policyExcess, :breakdownCover
+
 end
